054f9b81d26b1c8d558b02494d59fc16c16f2f23
Henryforce <hj_serrano@hotmail.com>
2026-01-16 19:21:09 +0900

    Updated Boost, Amp and Delay parameters (#8)

--- Sources/KatanaGoData/Models.swift
+++ Sources/KatanaGoData/Models.swift
@@ -39,10 +39,6 @@ public enum SystemParameter: Sendable {
 
 /// Parameters for the Amplifier block and front panel.
 public enum AmpParameter: Sendable {
-  // case loopActive
-  // case routingScheme
-  // case sendLevel
-  // case returnLevel
   case ampType(AmpType)
   case gain(UInt8)
   case volume(UInt8)
@@ -84,14 +80,15 @@ public enum AmpType: UInt8, Sendable, CaseIterable {
 
 /// Parameters for the Booster effect.
 public enum BoostParameter: Sendable {
-  case type
-  case drive
-  case bottom
-  case tone
-  case soloSwitch
-  case soloLevel
-  case effectLevel
-  case directMix
+  case enable(Bool)
+  case type(BoostType)
+  case drive(UInt8)
+  case bottom(UInt8)
+  case tone(UInt8)
+  case soloSwitchEnable(Bool)
+  case soloLevel(UInt8)
+  case effectLevel(UInt8)
+  case directMix(UInt8)
 }
 
 /// Available booster types for the Katana GO.
@@ -206,12 +203,21 @@ public enum BassModFXType: UInt8, Sendable, CaseIterable {
 
 /// Parameters for the Delay effect.
 public enum DelayParameter: Sendable {
-  case type
-  case time
-  case feedback
-  case highCut
-  case effectLevel
-  case directMix
+  case enable(Bool)
+  case type(DelayType)
+  case time(UInt16)
+  case feedback(UInt8)
+  case highCut(DelayHighCutFrequency)
+  case effectLevel(UInt8)
+  case directMix(UInt8)
+  case modulationRate(UInt8)
+  case modulationDepth(UInt8)
+  case modulationSwitch(Bool)
+  case tapTimePercentage(UInt8)
+  case filterEnable(Bool)
+  case filter(DelayFilterRange)
+  case delayPhase(DelayPhase)
+  case feedbackPhase(DelayPhase)
 }
 
 /// Available delay types for the Katana GO.
@@ -224,6 +230,34 @@ public enum DelayType: UInt8, Sendable, CaseIterable {
   case tapeEcho = 0x08
   case modulate = 0x09
   case sde3000 = 0x0A
+}
+
+public enum DelayHighCutFrequency: UInt8, Sendable, CaseIterable {
+  case freq630Hz = 0x00
+  case freq800Hz = 0x01
+  case freq1kHz = 0x02
+  case freq1k25Hz = 0x03
+  case freq1k6Hz = 0x04
+  case freq2kHz = 0x05
+  case freq2k5Hz = 0x06
+  case freq3k15Hz = 0x07
+  case freq4kHz = 0x08
+  case freq5kHz = 0x09
+  case freq6k3Hz = 0x0A
+  case freq8kHz = 0x0B
+  case freq10kHz = 0x0C
+  case freq12k5Hz = 0x0D
+  case flat = 0x0E
+}
+
+public enum DelayFilterRange: UInt8, Sendable, CaseIterable {
+  case freq8kHz = 0x00
+  case freq17kHz = 0x01
+}
+
+public enum DelayPhase: UInt8, Sendable, CaseIterable {
+  case normal = 0x00
+  case inverted = 0x01
 }
 
 /// Parameters for the Reverb effect.
