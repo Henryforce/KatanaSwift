03f7032712f9435d19827768cf789dc3d6ad4f3a
Henryforce <hj_serrano@hotmail.com>
2026-01-18 11:42:01 +0900

    Updated Mod/Fx values

--- Sources/KatanaGoData/ModFx.swift
+++ Sources/KatanaGoData/ModFx.swift
@@ -31,39 +31,37 @@ public enum ModFxParameter: Sendable, Hashable {
   case dc30(DC30Parameter)
 }
 
-// TODO: manual QA these types.
-/// Available MOD types for the Katana GO.
+// TODO: re-audit these values.
+/// Available MOD/FX types for the Katana GO.
 public enum ModFxType: UInt8, Sendable, Hashable, CaseIterable {
-  case tWah = 0x00
-  case autoWah = 0x01
-  case wah = 0x02
-  case compressor = 0x03
-  case limiter = 0x04
-  case graphicEQ = 0x06
-  case paraEQ = 0x07
-  case guitarSim = 0x09
+  case twoByTwoChorus = 0x00
+  case flanger = 0x01
+  case phaser = 0x02
+  case uniV = 0x03
+  case tremolo = 0x04
+  case vibrato = 0x06
+  case rotary = 0x07
+  case ringMod = 0x09
   case slowGear = 0x0A
-  case waveSynth = 0x0C
-  case octave = 0x0E
-  case pitchShifter = 0x0F
-  case harmonist = 0x10
-  case acProcessor = 0x12
-  case phaser = 0x13
-  case flanger = 0x14
-  case tremolo = 0x15
-  case rotary = 0x16
-  case uniV = 0x17
-  case slicer = 0x19
-  case vibrato = 0x1A
-  case ringMod = 0x1B
-  case humanizer = 0x1C
-  case twoByTwoChorus = 0x1D
-  case acGuitarSim = 0x1F
-  case phaser90E = 0x23
-  case flanger117E = 0x24
-  case wah95E = 0x25
-  case delayChorus30 = 0x26
-  case heavyOctave = 0x27
+  case slicer = 0x0C
+  case comp = 0x0D
+  case limiter = 0x0E
+  case tWah = 0x0F
+  case autoWah = 0x10
+  case graphicEQ = 0x11
+  case parametricEQ = 0x12
+  case guitarSim = 0x13
+  case acSim = 0x14
+  case acousticPro = 0x15
+  case waveSynth = 0x16
+  case octave = 0x17
+  case heavyOctave = 0x18
+  case pitchShifter = 0x19
+  case harmonist = 0x1A
+  case humanizer = 0x1B
+  case phaser90E = 0x1C
+  case flanger117E = 0x1D
+  case dc30 = 0x1E
 }
 
 // MARK: - Chorus
